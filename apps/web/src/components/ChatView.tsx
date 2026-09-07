31c512447fd86ce7e20b85b4f53b609031d0e285
rsmdevlin <bacapone@gmail.com>
2026-09-07 18:16:03 +0200

    Fix message group container for sticky avatars

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -915,7 +915,7 @@ export default function ChatView({
                 const isMine = group.senderId === user?.id;
 
                 return (
-                  <div key={`group-${groupIndex}-${group.senderId}`} className="relative min-h-[3rem]">
+                  <div key={`group-${groupIndex}-${group.senderId}`}>
                     {group.messages.map((msg, msgIndex) => {
                       const isGroupStart = msgIndex === 0;
                       const isGroupEnd = msgIndex === group.messages.length - 1;
